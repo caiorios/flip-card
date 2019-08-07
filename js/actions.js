ee05474e82410ac188e8f6821ab9f005616ff97d
Caio Rios de Souza <caiosouza@mpes.mp.br>
2019-08-07 16:22:43 -0300

    Retirado codigos nao utilizados. Corrigido transparencia no Safari

--- js/actions.js
+++ js/actions.js
@@ -3,15 +3,4 @@ function flipCard() {
   var cardFront = document.querySelector(".card-front");
   var cardBack = document.querySelector(".card-back");
   cardInner.classList.toggle("flip");
-  // cardBack.classList.toggle("show");
-  // cardFront.classList.toggle("hide");
-}
-
-function unflipCard() {
-  var cardInner = document.querySelector(".card-inner");
-  var cardFront = document.querySelector(".card-front");
-  var cardBack = document.querySelector(".card-back");
-  cardInner.classList.toggle("flip");
-  // cardFront.classList.toggle("hide");
-  // cardBack.classList.toggle("show");
 }
